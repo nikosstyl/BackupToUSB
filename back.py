e85ad108c82b75961a89fb589ffc061adde6d13d
Nikos Stylianou <68811067+nikosstyl@users.noreply.github.com>
2021-07-22 19:36:46 +0300

    Better security with file handling

--- back.py
+++ back.py
@@ -1,13 +1,14 @@
 from genericpath import getctime
 import shutil
+import platform
 from datetime import datetime
 import os
 
 def backup() :
-	print('Welcome to backup!\n');
 	indir = # Directory to be zipped!
 	loc = open('default.location', 'r');
 	loc.seek(0, 0);
+	# outdir format has to be like this: D:/path/to/file.zip
 	outdir = str(loc.read());
 	loc.close();
 	if os.path.exists(outdir) == False:
@@ -28,17 +29,30 @@ def backup() :
 		return(False);
 
 	print('USB found!');
-	tempdir = # A basic temporary directory
+	print('-------------- Info --------------')
+	tempdir = # A basic temporary directory.
 	shutil.make_archive(tempdir, 'zip', indir);
+	print('\tFolder zipped in temporary location')
 	if os.path.exists(outdir) == True:
-		os.rename(outdir, outdir[0:6]+'(1).zip')
-		os.remove(outdir[0:6]+'(1).zip')
+		os.rename(outdir, outdir[0:outdir.find('.zip')]+'(1).zip')
+		print('\tRenamed existing backup zip file')
+		mflag = True
 
-	shutil.move(tempdir+'.zip', outdir.replace(filename, '')) # Replace filename with your actual output filename. E.g., filename='MyZip.zip'
+	# shutil.move(tempdir+'.zip', outdir.replace(filename, '')) # Replace filename with your actual output filename. E.g., filename='MyZip.zip'
+	shutil.move(tempdir+'.zip', outdir[0:outdir.find('.zip')])
+	print('\tCopied new backup zip file')
+	if mflag == True:
+		os.remove(outdir[0:outdir.find('.zip')]+'(1).zip')
+		print('\tRemoved old backup zip file')
+	print('--------------- End --------------')
 	return (True);
 
 
 while 1:
+	if platform.system() == 'Windows':
+		os.system('clc') # Assuming that it has been called from cmd
+	elif platform.system() == 'Linux':
+		os.system('clear')
 	temp = backup();
 	if temp == True:
 		exit();
